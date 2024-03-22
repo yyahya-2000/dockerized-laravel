acdcc2e25222e823e3082498d5eb577975b562d7
yyahya-2000 <yanal.alyhya@gmail.com>
2024-03-22 23:20:28 +0300

    create routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProxyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(ProxyController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::post('/proxy/check', 'checkProxies')->name('proxy.check');
+    Route::get('/proxy/check/progress', 'showProgress')->name('checking.progress');
 });
